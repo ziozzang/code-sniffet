f95af32f6e004df754367de778f37bcc2ad7c79e
Jioh Jung <ziozzang@nexon.co.kr>
2013-03-19 16:43:25 +0900

    .

--- python/processlist.py
+++ python/processlist.py
@@ -1,3 +1,14 @@
+# process from /proc
+import os
+
+def get_all_process():
+  d = {}
+  pids= [pid for pid in os.listdir('/proc') if pid.isdigit()]
+  for pid in pids:
+    print open(os.path.join('/proc', pid, 'cmdline'), 'rb').read()
+
+
+# process list from psutil
 import psutil
 
 # 프로세스 목록을 얻어옴
